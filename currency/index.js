1df3ee45a58dd4df73cea515d0ec1527a10c3d03
TMG <69436315+CONTIGEN@users.noreply.github.com>
2021-01-29 09:36:17 +0100

    updated file structure

---
+++ currency/index.js
@@ -0,0 +1,132 @@
+function converter(num) {
+  document.getElementById("usd").value = (num / 380.5).toFixed(2);
+  document.getElementById("eur").value = (num / 453.46).toFixed(2);
+  document.getElementById("jpy").value = (num / 366).toFixed(2);
+  document.getElementById("gbp").value = (num / 494.7).toFixed(2);
+  document.getElementById("chf").value = (num / 409.58).toFixed(2);
+  document.getElementById("cad").value = (num / 290.03).toFixed(2);
+  document.getElementById("zar").value = (num / 23.45).toFixed(2);
+  document.getElementById("aed").value = (num / 104.28).toFixed(2);
+  document.getElementById("kwd").value = (num / 1254.05).toFixed(2);
+  document.getElementById("cny").value = (num / 56.58).toFixed(2);
+}
+function fromDollar(num) {
+  document.getElementById("naira").value = (num * 380.5).toFixed(2);
+  document.getElementById("eur").value = (num * 0.86).toFixed(2);
+  document.getElementById("jpy").value = (num * 105.59).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.78).toFixed(2);
+  document.getElementById("chf").value = (num * 0.93).toFixed(2);
+  document.getElementById("cad").value = (num * 1.34).toFixed(2);
+  document.getElementById("zar").value = (num * 17.1).toFixed(2);
+  document.getElementById("aed").value = (num * 3.67).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.31).toFixed(2);
+  document.getElementById("cny").value = (num * 6.82).toFixed(2);
+}
+function fromEuro(num) {
+  document.getElementById("naira").value = (num * 442.58).toFixed(2);
+  document.getElementById("jpy").value = (num * 122.82).toFixed(2);
+  document.getElementById("usd").value = (num * 1.16).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.91).toFixed(2);
+  document.getElementById("chf").value = (num * 1.08).toFixed(2);
+  document.getElementById("cad").value = (num * 1.56).toFixed(2);
+  document.getElementById("zar").value = (num * 19.89).toFixed(2);
+  document.getElementById("aed").value = (num * 4.27).toFixed(2);
+  document.getElementById("kwd").value = (num * 036).toFixed(2);
+  document.getElementById("cny").value = (num * 7.94).toFixed(2);
+}
+function fromYen(num) {
+  document.getElementById("naira").value = (num * 3.6).toFixed(2);
+  document.getElementById("eur").value = (num * 0.0081).toFixed(2);
+  document.getElementById("usd").value = (num * 0.0095).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.0074).toFixed(2);
+  document.getElementById("chf").value = (num * 0.0088).toFixed(2);
+  document.getElementById("cad").value = (num * 0.013).toFixed(2);
+  document.getElementById("zar").value = (num * 0.16).toFixed(2);
+  document.getElementById("aed").value = (num * 0.035).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.0029).toFixed(2);
+  document.getElementById("cny").value = (num * 0.065).toFixed(2);
+}
+function fromPounds(num) {
+  document.getElementById("naira").value = (num * 484.95).toFixed(2);
+  document.getElementById("eur").value = (num * 1.1).toFixed(2);
+  document.getElementById("jpy").value = (num * 134.61).toFixed(2);
+  document.getElementById("usd").value = (num * 1.27).toFixed(2);
+  document.getElementById("chf").value = (num * 1.18).toFixed(2);
+  document.getElementById("cad").value = (num * 1.71).toFixed(2);
+  document.getElementById("zar").value = (num * 21.8).toFixed(2);
+  document.getElementById("aed").value = (num * 4.68).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.39).toFixed(2);
+  document.getElementById("cny").value = (num * 8.7).toFixed(2);
+}
+function fromFranc(num) {
+  document.getElementById("naira").value = (num * 409.58).toFixed(2);
+  document.getElementById("eur").value = (num * 0.93).toFixed(2);
+  document.getElementById("jpy").value = (num * 113.66).toFixed(2);
+  document.getElementById("usd").value = (num * 1.08).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.84).toFixed(2);
+  document.getElementById("cad").value = (num * 1.44).toFixed(2);
+  document.getElementById("zar").value = (num * 18.41).toFixed(2);
+  document.getElementById("aed").value = (num * 3.95).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.33).toFixed(2);
+  document.getElementById("cny").value = (num * 7.35).toFixed(2);
+}
+function fromCad(num) {
+  document.getElementById("naira").value = (num * 267.37).toFixed(2);
+  document.getElementById("eur").value = (num * 0.64).toFixed(2);
+  document.getElementById("jpy").value = (num * 78.87).toFixed(2);
+  document.getElementById("usd").value = (num * 0.75).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.59).toFixed(2);
+  document.getElementById("chf").value = (num * 0.69).toFixed(2);
+  document.getElementById("zar").value = (num * 12.77).toFixed(2);
+  document.getElementById("aed").value = (num * 2.74).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.23).toFixed(2);
+  document.getElementById("cny").value = (num * 5.1).toFixed(2);
+}
+function fromZar(num) {
+  document.getElementById("naira").value = (num * 22.25).toFixed(2);
+  document.getElementById("eur").value = (num * 0.05).toFixed(2);
+  document.getElementById("jpy").value = (num * 6.17).toFixed(2);
+  document.getElementById("usd").value = (num * 0.058).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.046).toFixed(2);
+  document.getElementById("chf").value = (num * 0.054).toFixed(2);
+  document.getElementById("cad").value = (num * 0.078).toFixed(2);
+  document.getElementById("aed").value = (num * 0.21).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.0018).toFixed(2);
+  document.getElementById("cny").value = (num * 0.4).toFixed(2);
+}
+function fromAed(num) {
+  document.getElementById("naira").value = (num * 103.59).toFixed(2);
+  document.getElementById("eur").value = (num * 0.23).toFixed(2);
+  document.getElementById("jpy").value = (num * 28.75).toFixed(2);
+  document.getElementById("usd").value = (num * 0.27).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.21).toFixed(2);
+  document.getElementById("chf").value = (num * 0.25).toFixed(2);
+  document.getElementById("cad").value = (num * 0.36).toFixed(2);
+  document.getElementById("zar").value = (num * 4.66).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.083).toFixed(2);
+  document.getElementById("cny").value = (num * 1.86).toFixed(2);
+}
+function fromDinar(num) {
+  document.getElementById("naira").value = (num * 1241.6).toFixed(2);
+  document.getElementById("eur").value = (num * 2.81).toFixed(2);
+  document.getElementById("jpy").value = (num * 344.55).toFixed(2);
+  document.getElementById("usd").value = (num * 3.26).toFixed(2);
+  document.getElementById("gbp").value = (num * 2.56).toFixed(2);
+  document.getElementById("chf").value = (num * 3.03).toFixed(2);
+  document.getElementById("cad").value = (num * 4.37).toFixed(2);
+  document.getElementById("zar").value = (num * 55.8).toFixed(2);
+  document.getElementById("aed").value = (num * 11.99).toFixed(2);
+  document.getElementById("cny").value = (num * 22.27).toFixed(2);
+}
+function fromYuan(num) {
+  document.getElementById("naira").value = (num * 55.76).toFixed(2);
+  document.getElementById("eur").value = (num * 0.13).toFixed(2);
+  document.getElementById("jpy").value = (num * 15.47).toFixed(2);
+  document.getElementById("usd").value = (num * 0.15).toFixed(2);
+  document.getElementById("gbp").value = (num * 0.11).toFixed(2);
+  document.getElementById("chf").value = (num * 0.14).toFixed(2);
+  document.getElementById("cad").value = (num * 0.2).toFixed(2);
+  document.getElementById("zar").value = (num * 2.51).toFixed(2);
+  document.getElementById("aed").value = (num * 0.54).toFixed(2);
+  document.getElementById("kwd").value = (num * 0.045).toFixed(2);
+}
